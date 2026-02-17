65c912709ef042acfb5aab119e11676da8fb8e94
Lai Wei <lai.wei@enovation.ie>
2026-02-17 16:12:49 +0000

    Fix error in plugin version file

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_oauth2';
-$plugin->release      = '4.5.3';
-$plugin->version      = 2025041403;
-$plugin->requires     = 2025041400;
+$plugin->release      = '4.5.4';
+$plugin->version      = 2025041404;
+$plugin->requires     = 2024100700;
 $plugin->supported    = [405, 405];
 $plugin->maturity     = MATURITY_STABLE;
