07d9bcb2903964be3455cce11af7dd5b44461408
Lai Wei <lai.wei@enovation.ie>
2026-08-13 14:30:54 +0100

    Upgrade the plugin to support Moodle 5.2

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_oauth2';
-$plugin->release      = '5.1.2';
-$plugin->version      = 2025100602;
-$plugin->requires     = 2025100600;
-$plugin->supported    = [501, 501];
+$plugin->release      = '5.2.0';
+$plugin->version      = 2026042000;
+$plugin->requires     = 2026042000;
+$plugin->supported    = [502, 502];
 $plugin->maturity     = MATURITY_STABLE;
