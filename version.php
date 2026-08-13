e8bf0147dcad111141d295d164384d9e2bb850a0
Lai Wei <lai.wei@enovation.ie>
2026-08-13 16:11:01 +0100

    Update plugin version for 5.0.5 release

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_oauth2';
-$plugin->release      = '5.0.4';
-$plugin->version      = 2025041504;
+$plugin->release      = '5.0.5';
+$plugin->version      = 2025041505;
 $plugin->requires     = 2025041400;
 $plugin->supported    = [500, 500];
 $plugin->maturity     = MATURITY_STABLE;
